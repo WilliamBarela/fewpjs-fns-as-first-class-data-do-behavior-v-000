2b7d19154447ea3a6d861ac449947bd9c360ba8a
William Barela <william.barela.dev@gmail.com>
2020-01-22 22:51:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,8 +26,6 @@ function greet(timeString){
 
 /* Write your implementation of displayMessage() */
 function displayMessage(message){
- // let greeting = document.querySelector('h1#greeting');
- // greeting.innerHTML = message;
- let greeting = document.getElementById("greeting");
- greeting.innerHTML(message);
+ let greeting = document.querySelector('h1#greeting');
+ greeting.innerHTML = message;
 }
